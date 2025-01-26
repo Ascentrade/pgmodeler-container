0f09a36c8ffd3d95471b4222ef9a0fe57b01e02c
Dennis Greguhn <dev@greguhn.de>
2025-01-26 12:38:51 +0100

    Add network=host for X11
    Signed-off-by: Dennis Greguhn <dev@greguhn.de>

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ if [[ "${volume}" =~ ^(\/{0,1})[A-Za-z0-9\/\-_]+$ ]]
 then
     if [[ $(echo $XDG_SESSION_TYPE) == "x11" ]]
     then
-        docker run --rm --interactive --tty --security-opt label=disable --volume /tmp/.X11-unix/:/tmp/.X11-unix/ --volume "${volume}":/mnt/ --env DISPLAY pgmodeler
+        docker run --rm --interactive --tty --security-opt label=disable --volume /tmp/.X11-unix/:/tmp/.X11-unix/ --network host --volume "${volume}":/mnt/ --env DISPLAY pgmodeler
 
     elif [[ $(echo $XDG_SESSION_TYPE) == "wayland" ]]
     then
